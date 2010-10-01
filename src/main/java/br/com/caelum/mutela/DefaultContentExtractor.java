0378e6c3c081652f46648e71f9f0cf1c03266bc5
Paulo Silveira <paulo.silveira@caelum.com.br>
2010-10-01 19:41:25 -0300

    puxando content

--- src/main/java/br/com/caelum/mutela/DefaultContentExtractor.java
+++ src/main/java/br/com/caelum/mutela/DefaultContentExtractor.java
@@ -22,18 +22,19 @@ public class DefaultContentExtractor implements ContentExtractor {
 	private final String fullContent;
 
 	private Map<Element, Integer> elementToScore = new HashMap<Element, Integer>();
-	
+
 	private static Logger log = LoggerFactory.getLogger(DefaultContentExtractor.class);
 
 	// adaptado do Readability
-	public static final  String unlikelyCandidates = "(?i)(?m).*[combx|comment|community|disqus|extra|foot|header|menu|remark|rss|shoutbox|sidebar|sponsor|agegate|pagination|pager|popup].*";
-	public  static final String okMaybeItsACandidate = "(?i)(?m).*[and|article|body|column|main|shadow].*";
+	public static final String unlikelyCandidates = "(?i)(?m).*[combx|comment|community|disqus|extra|foot|header|menu|remark|rss|shoutbox|sidebar|sponsor|agegate|pagination|pager|popup].*";
+	public static final String okMaybeItsACandidate = "(?i)(?m).*[and|article|body|column|main|shadow].*";
 	public static final String positive = "(?i)(?m).*[article|body|content|entry|hentry|main|page|pagination|post|text|blog|story].*";
 	public static final String negative = "(?i)(?m).*[combx|comment|com-|contact|foot|footer|footnote|masthead|media|meta|outbrain|promo|related|scroll|shoutbox|sidebar|sponsor|shopping|tags|tool|widget].*";
 	public static final String divToPElements = "(?i)(?m).*<[a|blockquote|dl|div|ol|p|pre|table|ul].*";
 
 	private List<Element> elements;
-	
+
+	private String content;
 
 	public DefaultContentExtractor(String fullContent) {
 		this.fullContent = fullContent;
@@ -43,21 +44,21 @@ public class DefaultContentExtractor implements ContentExtractor {
 		for (Iterator<Element> i = elements.iterator(); i.hasNext();) {
 			Element e = i.next();
 			int weight = getValueForTag(e.getStartTag());
-			
-			if (matches(e.getAttributeValue("class"), positive) || matches(e.getAttributeValue("id"), positive)  ) {
-				weight +=50;
+
+			if (matches(e.getAttributeValue("class"), positive) || matches(e.getAttributeValue("id"), positive)) {
+				weight += 50;
 			}
 			if (matches(e.getAttributeValue("class"), negative) || matches(e.getAttributeValue("id"), negative)) {
-				weight -=50;
+				weight -= 50;
 			}
 			if (matches(e.getAttributeValue("class"), okMaybeItsACandidate)) {
-				weight +=10;
+				weight += 10;
 			}
 			if (matches(e.getAttributeValue("class"), unlikelyCandidates)) {
-				weight -=10;
+				weight -= 10;
 			}
-			
-			if(canBeContent(e)) {
+
+			if (canBeContent(e)) {
 				weight += e.toString().split(",").length;
 				addScore(e.getParentElement(), weight);
 				i.remove();
@@ -66,55 +67,61 @@ public class DefaultContentExtractor implements ContentExtractor {
 			}
 			System.out.println(weight);
 		}
-		
+
 		Collections.sort(elements, new Comparator<Element>() {
 			public int compare(Element o1, Element o2) {
 				return elementToScore.get(o1) - elementToScore.get(o2);
 			}
 		});
-		
+
 		Collections.reverse(elements);
 
-		log.info("Total de " + elements.size()+ " elementos ");
+		log.info("Total de " + elements.size() + " elementos ");
 
-		for(Element e : elements.subList(0, 3)) {
+		for (Element e : elements.subList(0, 3)) {
 			log.info("======================== Score deste: " + elementToScore.get(e));
 			log.info(e.toString());
 		}
+		
+		this.content = elements.get(0).getTextExtractor().setIncludeAttributes(true).toString();
 	}
 
 	private void addScore(Element element, int weight) {
-		if(elementToScore.containsKey(element)) {
+		if (elementToScore.containsKey(element)) {
 			elementToScore.put(element, elementToScore.get(element) + weight);
-		}
-		else {
+		} else {
 			elementToScore.put(element, weight);
 		}
 	}
 
 	private int getValueForTag(StartTag tag) {
-		 String name = tag.getName().toUpperCase();
-		 if(name.equals("DIV")) return 10;
-		 if("PRE TD BLOCKQUOTE".contains(name)) return 5;
-		 if("ADDRESS OL UL DL DD DT LI FORM".contains(name)) return -5;
-		 if("H1 H2 H3 H4 H5 H6 TH".contains(name)) return -10;
+		String name = tag.getName().toUpperCase();
+		if (name.equals("DIV"))
+			return 10;
+		if ("PRE TD BLOCKQUOTE".contains(name))
+			return 5;
+		if ("ADDRESS OL UL DL DD DT LI FORM".contains(name))
+			return -5;
+		if ("H1 H2 H3 H4 H5 H6 TH".contains(name))
+			return -10;
 		return 0;
 	}
 
 	private boolean canBeContent(Element e) {
-		return e.getName() == HTMLElementName.P ;//|| (e.getName() == HTMLElementName.DIV && !e.getContent().toString().matches(divToPElements));
+		return e.getName() == HTMLElementName.P;// || (e.getName() ==
+												// HTMLElementName.DIV &&
+												// !e.getContent().toString().matches(divToPElements));
 	}
 
 	private boolean matches(String value, String regex) {
-		if(value == null) return false;
+		if (value == null)
+			return false;
 		return value.matches(regex);
 	}
 
-
 	@Override
 	public String content() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.content;
 	}
 
 	@Override
